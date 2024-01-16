1674e6dbf1e69e45c6b083b11a7104befdfbf41b
marcocareddu <saldumaccu@tuta.io>
2024-01-16 11:32:33 +0100

    Added functions

--- src/App.js
+++ src/App.js
@@ -5,15 +5,42 @@ import './App.css';
 
 function App() {
   const [value, setValue] = useState(0);
+  const [title, setTitle] = useState("Titolo basic");
+  const [paragraph, setParagraph] = useState("Paragrafo basic");
 
-  const addCount = () => { setValue(value + 1) };
+  // Funzioni dichiarate dentro un oggetto per passare solo l'oggetto come props.
+  const functions = {
+    addCount: () => { setValue(value + 1) },
+    changeTitle: () => { setTitle("Titolo nuovo") },
+    changeParagraph: () => { setParagraph("Paragrafo nuovo") },
+    reset: () => {
+      setValue(0) 
+      setTitle("Titolo basic")
+      setParagraph("Paragrafo basic")
+    }
+  }
+
+  // Funzioni separate, in questo modo dovrei passare ogni singola funzione come props.
+  
+  // const addCount = () => { setValue(value + 1) };
+  // const changeTitle = () => { setTitle("Titolo nuovo") };
+  // const changeParagraph = () => { setParagraph("Paragrafo nuovo") };
+  // const reset = () => {
+  //   setValue(0) 
+  //   setTitle("Titolo basic")
+  //   setParagraph("Paragrafo basic")
+  // }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hai cliccato {value} volte</h1>
-        <Button addCount={addCount}/>
+        <h1>{title}</h1>
+        <h2>Hai cliccato {value} volte</h2>
+        <p>{paragraph}</p>
+
+        {/* Passo direttamente un oggetto contenente tutte le funzioni dichiarate */}
+        <Button {...functions}/>
       </header>
     </div>
   );
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Button = ({addCount}) => {
-  return (
-    <div>
-      <button onClick={() => addCount()}>Clicca qui</button>
-    </div>
-  )
-}
+const Button = ({reset, addCount, changeTitle, changeParagraph}) => {
+    return (
+      <div>
+        <button onClick={() => addCount()}>Aumenta il contatore</button>
+        <button onClick={() => changeTitle()}>Cambia Titolo</button>
+        <button onClick={() => changeParagraph()}>Cambia Paragrafo</button>
+        <button onClick={() => reset()}>Reset</button>
+      </div>
+    )
+  }
 
 export default Button
